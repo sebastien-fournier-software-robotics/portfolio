c6c120ba8821a204bd377c080df88a1e91ca0c0f
Seb Fournier <seb.fournier.tech@proton.me>
2026-02-14 18:36:33 +0100

    navbar.js file indentation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,131 +7,118 @@ import Button from "react-bootstrap/Button";
 import { CgGitFork } from "react-icons/cg";
 import { ImBlog } from "react-icons/im";
 import {
-  AiFillStar,
-  AiOutlineHome,
-  AiOutlineFundProjectionScreen,
-  AiOutlineUser,
+    AiFillStar,
+    AiOutlineHome,
+    AiOutlineFundProjectionScreen,
+    AiOutlineUser,
 } from "react-icons/ai";
 
 import { CgFileDocument } from "react-icons/cg";
 
 function scrollToSection(e, sectionId) {
-  e.preventDefault();
-  const el = document.getElementById(sectionId);
-  if (el) {
-    el.scrollIntoView({ behavior: "smooth", block: "nearest" });
-  }
+    e.preventDefault();
+    const el = document.getElementById(sectionId);
+    if (el) {
+        el.scrollIntoView({ behavior: "smooth", block: "nearest" });
+    }
 }
 
 function NavBar() {
-  const [expand, updateExpanded] = useState(false);
-  const [navColour, updateNavbar] = useState(false);
+    const [expand, updateExpanded] = useState(false);
+    const [navColour, updateNavbar] = useState(false);
 
-  function scrollHandler() {
-    if (window.scrollY >= 20) {
-      updateNavbar(true);
-    } else {
-      updateNavbar(false);
+    function scrollHandler() {
+        if (window.scrollY >= 20) {
+            updateNavbar(true);
+        } else {
+            updateNavbar(false);
+        }
     }
-  }
 
-  window.addEventListener("scroll", scrollHandler);
+    window.addEventListener("scroll", scrollHandler);
 
-  return (
-    <Navbar
-      expanded={expand}
-      fixed="top"
-      expand="md"
-      className={navColour ? "sticky" : "navbar"}
-    >
-      <Container>
-        <Navbar.Brand
-          href="#home"
-          className="d-flex"
-          onClick={(e) => scrollToSection(e, "home")}
+    return (
+        <Navbar
+            expanded={expand}
+            fixed="top"
+            expand="md"
+            className={navColour ? "sticky" : "navbar"}
         >
-          <img src={logo} className="img-fluid logo" alt="brand" />
-        </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="responsive-navbar-nav"
-          onClick={() => {
-            updateExpanded(expand ? false : "expanded");
-          }}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto" defaultActiveKey="#home">
-            <Nav.Item>
-              <Nav.Link
-                href="#home"
-                onClick={(e) => {
-                  scrollToSection(e, "home");
-                  updateExpanded(false);
-                }}
-              >
-                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
-              </Nav.Link>
-            </Nav.Item>
+            <Container>
+                <Navbar.Brand
+                    href="#home"
+                    className="d-flex"
+                    onClick={(e) => scrollToSection(e, "home")}
+                >
+                    <img src={logo} className="img-fluid logo" alt="brand" />
+                </Navbar.Brand>
+                <Navbar.Toggle
+                    aria-controls="responsive-navbar-nav"
+                    onClick={() => {
+                        updateExpanded(expand ? false : "expanded");
+                    }}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto" defaultActiveKey="#home">
+                        <Nav.Item>
+                            <Nav.Link
+                                href="#about"
+                                onClick={(e) => {
+                                    scrollToSection(e, "about");
+                                    updateExpanded(false);
+                                }}
+                            >
+                                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
+                            </Nav.Link>
+                        </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                href="#about"
-                onClick={(e) => {
-                  scrollToSection(e, "about");
-                  updateExpanded(false);
-                }}
-              >
-                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
-              </Nav.Link>
-            </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link
+                                href="#project"
+                                onClick={(e) => {
+                                    scrollToSection(e, "project");
+                                    updateExpanded(false);
+                                }}
+                            >
+                                <AiOutlineFundProjectionScreen
+                                    style={{ marginBottom: "2px" }}
+                                />{" "}
+                                Projects
+                            </Nav.Link>
+                        </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                href="#project"
-                onClick={(e) => {
-                  scrollToSection(e, "project");
-                  updateExpanded(false);
-                }}
-              >
-                <AiOutlineFundProjectionScreen
-                  style={{ marginBottom: "2px" }}
-                />{" "}
-                Projects
-              </Nav.Link>
-            </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link
+                                href="#resume"
+                                onClick={(e) => {
+                                    scrollToSection(e, "resume");
+                                    updateExpanded(false);
+                                }}
+                            >
+                                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+                            </Nav.Link>
+                        </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                href="#resume"
-                onClick={(e) => {
-                  scrollToSection(e, "resume");
-                  updateExpanded(false);
-                }}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
 
-            
-
-            <Nav.Item className="fork-btn">
-              <Button
-                href="https://github.com/soumyajit4419/Portfolio"
-                target="_blank"
-                className="fork-btn-inner"
-              >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />
-              </Button>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+                        <Nav.Item className="fork-btn">
+                            <Button
+                                href="https://github.com/soumyajit4419/Portfolio"
+                                target="_blank"
+                                className="fork-btn-inner"
+                            >
+                                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
+                                <AiFillStar style={{ fontSize: "1.1em" }} />
+                            </Button>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default NavBar;

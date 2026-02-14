cfc0bf5f39d14a5b1ce523ac41ea45b56ed07ca1
Seb Fournier <seb.fournier.tech@proton.me>
2026-02-14 20:58:55 +0100

    Change of picture in Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
+import homeLogo from "../../Assets/robot_dev.png";
+import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
 import Techstack from "../About/Techstack";
@@ -16,11 +17,12 @@ function Home() {
     return (
         <section>
             <Container fluid className="home-section" id="home">
+                <Particle />
                 <Container className="home-content">
-                    <Row>
+                    <Row className="align-items-center">
                         <Col md={7} className="home-header">
                             <h1 style={{ paddingBottom: 15 }} className="heading">
-                                Hello There!{" "}
+                                Hi There!{" "}
                                 <span className="wave" role="img" aria-labelledby="wave">
                                     👋🏻
                                 </span>
@@ -36,12 +38,24 @@ function Home() {
                             </div>
                         </Col>
 
-                        <Col md={5} style={{ paddingBottom: 20 }}>
+                        <Col
+                            md={5}
+                            style={{
+                                paddingBottom: 20,
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                overflow: "visible",
+                            }}
+                        >
                             <img
                                 src={homeLogo}
                                 alt="home pic"
-                                className="img-fluid"
-                                style={{ maxHeight: "450px" }}
+                                style={{
+                                    height: "550px",
+                                    maxWidth: "none",
+                                    objectFit: "contain",
+                                }}
                             />
                         </Col>
                     </Row>

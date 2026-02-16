afd0393669718c3c7537442823ffa02cfede4adf
Seb Fournier <seb.fournier.tech@proton.me>
2026-02-16 14:20:16 +0100

    Update of Contact Me (text + icons + style + trad)

--- src/components/FindMeOn.js
+++ src/components/FindMeOn.js
@@ -9,46 +9,46 @@ function FindMeOn() {
     const { t } = useLanguage();
 
     return (
-        <Container id="contact">
-            <Row style={{ paddingTop: "50px", paddingBottom: "80px" }}>
-                <Col md={12} className="home-about-social">
-                    <h1>{t("findMeOn.title")}</h1>
-                    <p>
-                        {t("findMeOn.subtitle")}
-                    </p>
-                    <ul className="home-about-social-links">
-                        <li className="social-icons">
-                            <a
-                                href="mailto:seb.fournier.tech@proton.me"
-                                rel="noreferrer"
-                                className="icon-colour  home-social-icons"
-                            >
-                                <MdOutlineEmail />
-                            </a>
-                        </li>
-                        <li className="social-icons">
-                            <a
-                                href="https://www.linkedin.com/in/sebastien-fournier-software-robotics/"
-                                target="_blank"
-                                rel="noreferrer"
-                                className="icon-colour  home-social-icons"
-                            >
-                                <FaLinkedinIn />
-                            </a>
-                        </li>
-                        <li className="social-icons">
-                            <a
-                                href="https://github.com/sebastien-fournier-software-robotics"
-                                target="_blank"
-                                rel="noreferrer"
-                                className="icon-colour  home-social-icons"
-                            >
-                                <AiFillGithub />
-                            </a>
-                        </li>
-                    </ul>
-                </Col>
-            </Row>
+        <Container fluid className="contact-section" id="contact">
+            <Container>
+                <Row>
+                    <Col md={12} className="home-about-social">
+                        <h1 className="project-heading">{t("findMeOn.title")}</h1>
+                        <p className="contact-subtitle">{t("findMeOn.subtitle")}</p>
+                        <ul className="home-about-social-links">
+                            <li className="social-icons">
+                                <a
+                                    href="mailto:seb.fournier.tech@proton.me"
+                                    rel="noreferrer"
+                                    className="icon-colour  home-social-icons"
+                                >
+                                    <MdOutlineEmail />
+                                </a>
+                            </li>
+                            <li className="social-icons">
+                                <a
+                                    href="https://www.linkedin.com/in/sebastien-fournier-software-robotics/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="icon-colour  home-social-icons"
+                                >
+                                    <FaLinkedinIn />
+                                </a>
+                            </li>
+                            <li className="social-icons">
+                                <a
+                                    href="https://github.com/sebastien-fournier-software-robotics"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="icon-colour  home-social-icons"
+                                >
+                                    <AiFillGithub />
+                                </a>
+                            </li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
         </Container>
     );
 }
